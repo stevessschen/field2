f89f55fe9f350c8be2dd7f3cf4fc8b3243b7487e
stevessschen <36218346+stevessschen@users.noreply.github.com>
2021-11-02 11:46:19 +0800

    Update ssd_pcba_inference.py

--- ssd_pcba_inference.py
+++ ssd_pcba_inference.py
@@ -1,3 +1,5 @@
+import streamlit as st 
+
 import cv2
 import keras
 from keras.applications.imagenet_utils import preprocess_input
@@ -25,7 +27,10 @@ set_session(tf.Session(config=config))
 voc_classes = ['x','p','t']
 NUM_CLASSES = len(voc_classes) + 1
 
+st.write('Build ssd model...')
 input_shape=(300, 300, 3)
 model = SSD300(input_shape, num_classes=NUM_CLASSES)
+st.write('Load weight...')
 model.load_weights('./checkpoints/weights.04-2.13.hdf5', by_name=True)
 bbox_util = BBoxUtility(NUM_CLASSES)
+
